5cc02dd6687215b5f6830272cebe25733b3821d8
Manasvin5 <manasvinshrimali@gmail.com>
2024-06-20 22:06:56 +0530

    Second Commit

---
+++ install-scripts/dotfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 💫 https://github.com/Manasvin5 💫 #
+# Hyprland-Dots to download from Releases #
+if [[ $USE_PRESET = [Yy] ]]; then
+  source ./preset.sh
+fi
+
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/Manasvin5/hyprdote.git || exit 1
+
+# Change to the cloned directory
+cd hyprdote || exit 1
+
+# Set execute permission for copy.sh and execute it
+chmod +x copy.sh
+./copy.sh
+
+echo -e "${OK} Dotfiles cloned and processed successfully. Check hyprdote folder for more detailed install logs" 2>&1 | tee -a "../Install-Logs/install-$(date +'%d-%H%M%S')_dotfiles.log"
